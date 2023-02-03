1830482b1d73c7a957b33c374e5678502c93f9a4
CAAndinoS <124225358+CAAndinoS@users.noreply.github.com>
2023-02-02 22:58:05 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pm011p2023/transacciones/Transacciones.java
@@ -0,0 +1,16 @@
+package com.example.pm011p2023.transacciones;
+
+public class Transacciones {
+    //Nombre de la base de datos
+    public static final String NameDatabase ="PM01DB";
+    //Tablas de la base de datos
+    public static final String tablapersonas ="personas";
+
+    //Transacciones de PMDB
+    public static final String CreateTBPersonas =
+            "CREATE TABLE personas (id INTEGER PRIMARY KEY AUTOINCREMENT, nombres TEXT ," +
+            "apellidos TEXT, edad INTEGER, correo TEXT )";
+    public static final String DropTablePersonas = "DROP TABLE IF EXISTS personas";
+    public static final String Empty = "";
+
+}
